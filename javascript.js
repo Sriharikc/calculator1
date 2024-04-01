cb16098607a8fe40fa63d61d055aaa6ac8ea6695
Sriharikc <142884967+Sriharikc@users.noreply.github.com>
2024-04-01 17:51:00 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+function add(){
+    var firstOp, secondOp, result;
+  firstOp =  document.getElementById('input1').value;
+    secondOp =  document.getElementById('input2').value;
+    result = parseInt(firstOp) + parseInt(secondOp);
+     window.alert(result);
+   }
+     function add1(){
+       var firstOp, secondOp, result;
+     firstOp =  document.getElementById('input1').value;
+       secondOp =  document.getElementById('input2').value;
+       result = parseInt(firstOp) - parseInt(secondOp);
+        window.alert(result);
+      }
+        function add2(){
+          var firstOp, secondOp, result;
+        firstOp =  document.getElementById('input1').value;
+          secondOp =  document.getElementById('input2').value;
+          result = parseInt(firstOp) * parseInt(secondOp);
+           window.alert(result);
+         }
+           function add3(){
+             var firstOp, secondOp, result;
+           firstOp =  document.getElementById('input1').value;
+             secondOp =  document.getElementById('input2').value;
+             result = parseInt(secondOp) / parseInt(firstOp);
+              window.alert(result);
+   }
